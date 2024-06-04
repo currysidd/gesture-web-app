7e7009df172381b03572fcad9b68019daf900645
currysidd <159699021+currysidd@users.noreply.github.com>
2024-06-04 18:42:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -14,14 +14,45 @@ function takeSnapshot() {
     })
 }
 function predictEmotion() {
-    speak()
+ img=document.getElementById("image")
+    
+    classifier.classify(img,gotResult)
 }
+
+function gotResult(error,results){
+    if (error) {
+        console.error(error);   
+       } else {
+           console.log(results);
+           p1=results[0].label
+           p2=results[1].label
+           speak()
+           document.getElementById("emotionResult1").innerHTML=p1
+           if (p1=="thumbs up") {
+               document.getElementById("emojiResult1").innerHTML="👍"
+           }
+           if (p1=="nice") {
+               document.getElementById("emojiResult1").innerHTML="👌"
+           }
+           if (p1=="heart") {
+               document.getElementById("emojiResult1").innerHTML="🫶"
+           }
+           if (p1=="thumbs down") {
+               document.getElementById("emojiResult1").innerHTML="👎"
+           }
+           if (p1=="high 5") {
+            document.getElementById("emojiResult1").innerHTML="🖐️"
+        }
+           
+       }
+}
+
 console.log(ml5.version);
 function modelLoaded() {
     console.log("model loaded");
 }
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/mYbBdeq6x/.json", modelLoaded)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/mYbBdeq6x/model.json", modelLoaded)
 p1 = ""
 function speak() {
     synth = window.speechSynthesis
